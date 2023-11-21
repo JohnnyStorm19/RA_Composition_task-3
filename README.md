0a48d9c1aa74d4e393e7176baa5d760abde378f2
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-11-21 22:10:12 +0300

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# React_Composition_task-3
+
+## [Launch](https://johnnystorm19.github.io/RA_Forms_task-1/)
+
+
+
---
+++ src/components/Collapse.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export function Collapse({
+  collapsedLabel = true,
+}: {
+  collapsedLabel?: boolean;
+  expandedLabel?: boolean;
+}) {
+  const [collapse, setCollapse] = useState(collapsedLabel);
+// ?   const [expand, setExpand] = useState(expandedLabel); не очень понял зачем этот стэйт
+
+  const handleBtnClick = () => {
+    setCollapse(!collapse);
+  };
+  return (
+    <div className="collapse-container">
+      <button 
+        className="collapse-btn"
+        type="button" 
+        onClick={handleBtnClick}
+    >
+        Collapsing button
+      </button>
+        <div className={`collapse-content-container ${!collapse ? 'collapsed': ''}`}>
+            <div className="collapse-content">
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </div>
+    </div>
+  );
+}
